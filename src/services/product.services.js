a8bb2049125d12a183803aa7b434d4d482ad0b82
Fernando Rossler <frossl@Fernandos-MacBook-Pro.local>
2023-11-23 00:26:15 -0300

    check

---
+++ src/services/product.services.js
@@ -0,0 +1,42 @@
+import ProductDaoMongoDB from "../daos/mongodb/product.dao.js";
+const productDao = new ProductDaoMongoDB();
+
+export const getAll = async () => {
+    try {
+        
+    } catch (error) {
+        
+    };
+};
+
+export const getById = async (id) => {
+    try {
+        
+    } catch (error) {
+        
+    };
+};
+
+export const create = async (obj) => {
+    try {
+        
+    } catch (error) {
+        
+    };
+};
+
+export const update = async (id, obj) => {
+    try {
+        
+    } catch (error) {
+        
+    };
+};
+
+export const delete = async () => {
+    try {
+        
+    } catch (error) {
+        
+    };
+};
